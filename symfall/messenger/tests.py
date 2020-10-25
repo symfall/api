50623049708f3e3f93e3779b9017e88ee25b7955
swaroggot <70681981+swaroggot@users.noreply.github.com>
2020-10-25 18:48:43 +0300

    Add unittest for health check api endpoint (#32)
    * Add tests for Health Check viewset

--- symfall/messenger/tests.py
+++ symfall/messenger/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class HealthCheckViewTest(APITestCase):
+    def test_health_check(self):
+        """
+        Method for viewing health check in json format
+        """
+        response = self.client.get('/health_check/', data={'format': 'json'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual({
+            'Cache backend: default': 'working',
+            'DatabaseBackend': 'working',
+            'DiskUsage': 'working',
+            'MemoryUsage': 'working',
+            'MigrationsHealthCheck': 'working'},
+            response.json()
+        )
